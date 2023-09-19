c71dd18c8a5029b81835be12e61e5b53f1072870
Henry Navarro <henrymanuelnavarro@gmail.com>
2023-09-19 15:19:09 -0400

    [UPDATE] account_move

---
+++ models/account_move.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    control_number = fields.Char("Control Number", copy=False)
+
+    @api.constrains("control_number")
+    def check_control_number(self):
+        for move in self:
+            if move.control_number:
+                try:
+                    assert move.control_number.isdigit(), \
+                    _("The number of the fiscal correlative must be an entire digit")
+                except Exception as e:
+                    raise UserError(str(e))
